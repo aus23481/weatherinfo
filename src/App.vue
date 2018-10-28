77e5dddaa29a22703a8c8a26815df5b30e3604b1
Abbas <aus234@yahoo.com>
2018-10-28 08:34:35 +0600

    StarterKit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <weather/>
+    <footer-prismic/>
+  </div>
+</template>
+
+<script>
+import FooterPrismic from './components/FooterPrismic.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FooterPrismic
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/resetr.css";
+@import "./assets/css/prismic-edit-button.css";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>This is home pagedfdf</p>
+    
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Home'
+}
+</script>
